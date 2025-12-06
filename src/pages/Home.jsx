02370beeef0df3fa7a4ffc03a446b5acb4c9812b
longbtf8 <longbtf8@fullstack.edu.vn>
2025-12-06 16:32:45 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,20 +7,26 @@ const Home = () => {
   return (
     <>
       <h1 className="text-3xl p-4">Danh sách sản phẩm </h1>
-      {/* <ul className="p-4 flex gap-2">
-        <li>Tên</li>
-        <li>Giá</li>
-        <li>Hình ảnh</li>
-        <li>Mô tả ngắn</li>
-      </ul> */}
-      {/* {data.items.map((item) => {
-        <div>
-          <p>{item.title}</p>
-          <p>{item.price}</p>
-          <p>{item.description}</p>
-          <p>{item.thumbnail}</p>
-        </div>;
-      })} */}
+      <div className="p-4">
+        <ul className="flex  border-b-2">
+          <li className="w-1/4 border-r-2">Hình ảnh</li>
+          <li className="w-1/4 border-r-2">Tên Sản Phẩm </li>
+          <li className="w-1/4 border-r-2">Giá</li>
+          <li className="w-1/4 border-r-2">Mô tả ngắn</li>
+        </ul>
+        {isLoading
+          ? "Loading..."
+          : data.items.map((item) => (
+              <div key={item.id} className="flex border-b-2 ">
+                <p className="w-1/4 wrap-break-word border-r-2">
+                  {item.thumbnail}
+                </p>
+                <p className="w-1/4 border-r-2">{item.title}</p>
+                <p className="w-1/4 border-r-2 ">{item.price}</p>
+                <p className="w-1/4 border-r-2">{item.description}</p>
+              </div>
+            ))}
+      </div>
     </>
   );
 };
